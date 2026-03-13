7071a98fb6e8742c6f8414a0deb3fcc3c2cd65c4
banchodsala68-sys <banchodsala68@gmail.com>
2026-03-13 12:20:35 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram API credentials
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_ID = os.getenv('27099161')
+API_HASH = os.getenv('4ebbba630c8da1e27875ba399ae78a7f')
+BOT_TOKEN = os.getenv('7907973008:AAH6M39DXNRS8O0auaZbwFH7wkT19AC5Gw8')
 
 # Admin Telegram ID
-ADMIN_ID = int(os.getenv('ADMIN_ID', '0'))
+ADMIN_ID = int(os.getenv('5340147496', '0'))
 
 # Bot Settings
 BOT_NAME = "Premium Adult Content"
